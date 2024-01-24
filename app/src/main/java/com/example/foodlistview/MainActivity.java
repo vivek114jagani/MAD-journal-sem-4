8fcdc4ca5e60bcc0753dabbb14f45d9b3e9bb4b5
vivekjagani047@gmail.com <vivekjagani047@gmail.com>
2024-01-24 14:07:13 +0530

    DONE: First task complete

---
+++ app/src/main/java/com/example/foodlistview/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.foodlistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+
+import com.example.foodlistview.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    ListAdapter listAdapter;
+    ArrayList<ListData> dataArrayList = new ArrayList<>();
+    ListData listData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int[] imageList = {R.drawable.pasta, R.drawable.maggi, R.drawable.cake, R.drawable.pancake, R.drawable.pizza, R.drawable.burger, R.drawable.fries};
+        int[] ingredientList = {R.string.pastaIngredients, R.string.maggiIngredients, R.string.cakeIngredients, R.string.pancakeIngredients, R.string.pizzaIngredients, R.string.burgerIngredients, R.string.friesIngredients};
+        int[] descList = {R.string.pastaDesc, R.string.maggieDesc, R.string.cakeDesc, R.string.pancakeDesc, R.string.pizzaDesc, R.string.burgerDesc, R.string.friesDesc};
+        String[] nameList = {"Pasta", "Maggi", "Cake", "Pancake", "Pizza", "Burger", "Fries"};
+        String[] timeList = {"30 mins", "2 mins", "45 mins", "10mins", "60 mins", "45mins", "30mins"};
+
+        for (int i = 0; i < imageList.length; i++) {
+            listData = new ListData(nameList[i], timeList[i], ingredientList[i], descList[i], imageList[i]);
+            dataArrayList.add(listData);
+        }
+
+        listAdapter = new ListAdapter(MainActivity.this, dataArrayList);
+        binding.listView.setAdapter(listAdapter);
+        binding.listView.setClickable(true);
+
+        binding.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+                intent.putExtra("name", nameList[position]);
+                intent.putExtra("time", timeList[position]);
+                intent.putExtra("ingredients", ingredientList[position]);
+                intent.putExtra("desc", descList[position]);
+                intent.putExtra("image", imageList[position]);
+                startActivity(intent);
+            }
+        });
+    }
+}
